5631e069984eec4bbceabe9c594a768908c0af6d
dag.rende <dag.rende@355cb063-aff5-3a1e-408f-b5dec8ac1020>
2010-04-08 17:49:10 +0000

    now the source link in the about box can be clicked to open it in a browser.

--- src/se/rende/mytime/About.java
+++ src/se/rende/mytime/About.java
@@ -17,11 +17,15 @@
 package se.rende.mytime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.util.Linkify;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 /**
@@ -34,20 +38,31 @@ public class About extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
-		
+
 		TextView versionTextView = (TextView) findViewById(R.id.about_version);
 		String rev = "$Revision$";
 		try {
-			PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-			versionTextView.setText(getString(R.string.about_version) + " " + packageInfo.versionName + "." + rev.split(" ")[1]);
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(
+					getPackageName(), 0);
+			versionTextView.setText(getString(R.string.about_version) + " "
+					+ packageInfo.versionName + "." + rev.split(" ")[1]);
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		TextView srcLinkTextView = (TextView) findViewById(R.id.about_src_link);
-		SpannableString str = SpannableString.valueOf(srcLinkTextView.getText()); 
+		final String srcLinkText = srcLinkTextView.getText().toString();
+		SpannableString str = SpannableString
+				.valueOf(srcLinkTextView.getText());
 		Linkify.addLinks(str, Linkify.ALL);
 		srcLinkTextView.setText(str);
+		srcLinkTextView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(srcLinkText));
+				startActivity(myIntent);
+			}
+		});
 	}
 }
