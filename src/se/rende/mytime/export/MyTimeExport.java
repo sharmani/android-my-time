a445a585a2d1ba5b21cd8665143514548e1085d0
dag.rende <dag.rende@355cb063-aff5-3a1e-408f-b5dec8ac1020>
2010-12-26 18:34:27 +0000

    * exports to sd card only  * imports from clicked file in list of all sd files matching file name pattern  * removed main program launch and open from mail program  * both import and export are launched from My Time Other menu

--- src/se/rende/mytime/export/MyTimeExport.java
+++ src/se/rende/mytime/export/MyTimeExport.java
@@ -11,11 +11,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MyTimeExport extends Activity {
-	private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yy-MM-dd HH-mm");
+	private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yy-MM-dd HH-mm-ss");
 
 	/** Called when the activity is first created. */
 	@Override
@@ -36,17 +36,18 @@ public class MyTimeExport extends Activity {
 			FileOutputStream os = new FileOutputStream(backupFile);
 			backupFormatter.writeXml(os);
 			os.close();
+			Toast.makeText(this, "Saved '" + backupFile + "'", Toast.LENGTH_LONG).show();
 
 			Intent i = new Intent(android.content.Intent.ACTION_SEND);
 			i.putExtra(Intent.EXTRA_SUBJECT, fileName);
 			i.setType("text/xml");
 			i.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + backupFile));
 
-			startActivity(Intent.createChooser(i,
-					getString(R.string.export_title)));
+//			startActivity(Intent.createChooser(i,
+//					getString(R.string.export_title)));
 		} catch (Exception e) {
 			Log.e(getClass().getSimpleName(), "export error", e);
-//			new AlertDialog.Builder(this).setMessage("backup error " + e).show();
+			new AlertDialog.Builder(this).setMessage("backup error " + e).show();
 		}
 	}
 
